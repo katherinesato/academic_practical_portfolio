25c05ba35e3e433bd06a00f2d227ee4a63b5a2ec
Katherine Borges Sato <katherine.borges.sato@gmail.com>
2026-09-19 14:46:04 -0400

    add new coding resolution to portfolio

---
+++ coding_challenges/implementation_bag/listbag.py
@@ -0,0 +1,172 @@
+"""
+Project: Custom List-based Bag ADT (Abstract Data Type)
+
+Description:
+    An implementation of the Bag ADT (Multiset) using Python's built-in list data structure. 
+    A Bag is a collection that allows duplicate elements, focusing on custom data structure 
+    design and collection management.
+
+Author:
+    - Katherine Borges Sato
+
+Key Skills & Concepts Demonstrated:
+    - Object-Oriented Programming (OOP): Implementation of custom classes and dunder/magic 
+      methods (__init__, __len__, __add__, __eq__) for operator overloading and built-in function integration.
+    - Data Structures & Algorithms: Designing an Abstract Data Type (ADT), managing underlying lists, 
+      and implementing order-independent collection comparison (handling duplicates/multisets).
+    - Error & Exception Handling: Robust type checking and input validation using custom exceptions 
+      (ValueError, TypeError).
+    - Python Best Practices: Use of type hinting, clean code architecture, and standard library modules (random).
+"""
+
+import random
+
+
+class ListBag:
+
+    def __init__(self, iterable=[]) -> None:
+        """Initialize this ListBag.
+
+        If no iterable is provided, the new ListBag is empty.
+        Otherwise, initialize the ListBag by adding the values
+        provided by the iterable.
+
+        >>> bag = ListBag()
+        >>> bag
+        ListBag([])
+        >>> bag = ListBag([1, 4, 3, 6, 3])
+        >>> bag
+        ListBag([1, 4, 3, 6, 3])
+        """
+        self._elems = []
+        for item in iterable:
+            self._elems.append(item)
+            # or, self.add(item)
+
+    def __len__(self) -> int:
+        """Return the number of items in this ListBag."""
+        return len(self._elems)
+
+    def count(self, item: any) -> int:
+        """Return the total number of occurrences of item in this bag.
+
+        >>> bag = ListBag([3, 1, 2, 3, 4])
+        >>> bag.count(3)
+        2
+        >>> bag.count(7)
+        0
+        """
+        return self._elems.count(item)
+
+    def remove(self, item: any) -> any:
+        """Remove and return one instance of item from this ListBag.
+
+        Raises ValueError if the bag is empty.
+        Raises ValueError if item is not in the bag.
+
+        >>> bag = ListBag([3, 1, 2, 3, 4])
+
+        # The bag has 5 elements, including two 3's.
+        >>>len(bag)
+        5
+        >>> bag.count(3)
+        2
+
+        # Now remove one 3.
+        >>> bag.remove(3)
+        3
+        >>> bag.count(3)
+        1
+        >>> len(bag)
+        4
+        """
+        if self._elems == []:
+            raise ValueError("bag.remove(", item, "): remove from empty bag")
+        if item in self._elems:
+            index = self._elems.index(item)
+            return self._elems.pop(index)
+        raise ValueError("bag.remove(", item, "): x not in bag")
+
+    def grab(self) -> any:
+        """Remove and return a randomly selected item from this bag.
+
+        Raises ValueError if the bag is empty.
+
+        >>> bag = ListBag([3, 1, 2, 3, 4])
+        >>> len(bag)
+        5
+
+        >>> ListBag.grab()
+        # grab will randomly select one of items stored in the bag,
+        # and remove and return that value. The value displayed in the shell
+        # will be one of 1, 2, 3 or 4, depending on which item was removed.
+
+        >>> len(bag)
+        4
+        """
+        if self._elems == []:
+            raise ValueError("bag.grab(): grab from empty bag")
+        grab_item = random.choice(self._elems)
+        return self.remove(grab_item)
+
+    def __add__(self, other: 'ListBag') -> 'ListBag':
+        """Return a new ListBag containing the concatenation of self and other.
+
+        Raises TypeError if other is not a ListBag.
+
+        >>> bag1 = ListBag([1, 3, 5])
+        >>> bag2 = ListBag([2, 4, 6])
+        >>> bag3 = bag1 + bag2
+        >>> repr(bag3)
+        'ListBag([1, 3, 5, 2, 4, 6])'
+
+        Note: Depending on how __add__ and __repr__ are implemented, the
+        order of the elements in the string returned by repr may be different.
+        """
+        if not isinstance(other, ListBag):
+            raise TypeError("can only concatenate ListBag to ListBag")
+
+        return ListBag(self._elems + other._elems)
+
+    def __eq__(self, other: 'ListBag') -> bool:
+        """Return True if self is equal to the ListBag referred to by other;
+        otherwise return False.
+
+        >>> bag1 = ListBag([1, 2, 3])
+        >>> bag2 = ListBag([3, 2, 1])
+        >>> bag1 == bag2
+        True
+
+        >>> bag1 = ListBag([1, 2, 3])
+        >>> bag2 = ListBag([4, 5, 6])
+        >>> bag1 == bag2
+        False
+        """
+        if not isinstance(other, ListBag):
+            return False
+
+        # Solution considering that dict can also be an elem of ListBag:
+        if len(self._elems) != len(other._elems):
+            return False
+
+        other_elems_copy = list(other._elems)
+
+        for item in self._elems:
+            if item in other_elems_copy:
+                other_elems_copy.remove(item)
+            else:
+                return False
+
+        return True
+
+        # Another solution considering that there are no dict in the ListBag:
+        #
+        # list1 = list(self._elems)
+        # list2 = list(other._elems)
+        # list1.sort()
+        # list2.sort()
+        # if list1 == list2:
+        #   return True
+
+        # return False
+
---
+++ coding_challenges/implementation_bag/test_listbag.py
@@ -0,0 +1,265 @@
+import unittest
+
+from listbag import ListBag
+
+
+class CountTestCase(unittest.TestCase):
+
+    def test_count1(self):
+        """Test count with an empty bag """
+        bag = ListBag()
+        self.assertEqual(bag.count(3), 0)
+        self.assertEqual(bag.count(True), 0)
+        self.assertEqual(bag.count("Hi"), 0)
+        self.assertEqual(bag.count([]), 0)
+        self.assertEqual(bag.count([1, 2]), 0)
+
+    def test_count2(self):
+        """Test count with a bag containing int elements"""
+        bag = ListBag([2, 2, 2, 8, 9, 12, -15, -15, 27, 0, 0])
+        self.assertEqual(bag.count(2), 3)
+        self.assertEqual(bag.count(-15), 2)
+        self.assertEqual(bag.count(0), 2)
+        self.assertEqual(bag.count(8), 1)
+        self.assertEqual(bag.count(84), 0)
+
+    def test_count3(self):
+        """Test count with a bag containing float, 'double' elements"""
+        bag = ListBag([5.2, 5.2, -4.0, 4.0, 5.78493])
+        self.assertEqual(bag.count(5.2), 2)
+        self.assertEqual(bag.count(-4.0), 1)
+        self.assertEqual(bag.count(5.78493), 1)
+        self.assertEqual(bag.count(84.7), 0)
+
+    def test_count4(self):
+        """Test count with a bag containing char and string elements"""
+        bag = ListBag(['a', 'a', "This", "A", "A", "Katherine", '3.2451'])
+        self.assertEqual(bag.count("This"), 1)
+        self.assertEqual(bag.count('a'), 2)
+        self.assertEqual(bag.count("Katherine"), 1)
+        self.assertEqual(bag.count('3.2451'), 1)
+        self.assertEqual(bag.count("Hi"), 0)
+
+    def test_count5(self):
+        """Test count with a bag containing boolean elements"""
+        bag = ListBag([True, True, True])
+        self.assertEqual(bag.count(True), 3)
+        self.assertEqual(bag.count(False), 0)
+
+    def test_count6(self):
+        """Test count with a bag that contain lists, tuples, dict as elements"""
+        bag = ListBag([[1, 2], [], [], (3, 7), (3, 7),
+                       {1: 1, 2: 2}, {"Dad": 35, "Mom": 34}])
+        self.assertEqual(bag.count([1, 2]), 1)
+        self.assertEqual(bag.count([]), 2)
+        self.assertEqual(bag.count((3, 7)), 2)
+        self.assertEqual(bag.count({1: 1, 2: 2}), 1)
+        self.assertEqual(bag.count({"Dad": 35, "Mom": 34}), 1)
+
+
+class RemoveTestCase(unittest.TestCase):
+
+    def test_remove1(self):
+        """Test removing from an empty bag."""
+        bag = ListBag()
+        with self.assertRaises(ValueError):
+            bag.remove(10)
+
+    def test_remove2(self):
+        """Test removing an item that isn't in the bag."""
+        bag = ListBag([1, 3, 4, 4, 7, 2, 3])
+        with self.assertRaises(ValueError):
+            bag.remove(10)
+
+    def test_remove3(self):
+        """Test removing an item of value int."""
+        bag = ListBag([1, 3, 4, 4, 7, 2, 3])
+        self.assertEqual(bag.remove(4), 4)
+        self.assertEqual(bag.remove(7), 7)
+
+    def test_remove4(self):
+        """Test removing an item of value floar, 'double'."""
+        bag = ListBag([1.0, 3.7, 4.2, 4.2, 7.8, 2.79, 3.753])
+        self.assertEqual(bag.remove(4.2), 4.2)
+        self.assertEqual(bag.remove(3.753), 3.753)
+
+    def test_remove5(self):
+        """Test removing an item of value char, string."""
+        bag = ListBag(["Hi", "Hi", "A", 'a', '33'])
+        self.assertEqual(bag.remove("Hi"), "Hi")
+        self.assertEqual(bag.remove("A"), "A")
+        self.assertEqual(bag.remove('a'), 'a')
+        self.assertEqual(bag.remove('33'), '33')
+
+    def test_remove6(self):
+        """Test removing an item of value bool."""
+        bag = ListBag([True, False])
+        self.assertEqual(bag.remove(True), True)
+        self.assertEqual(bag.remove(False), False)
+
+    def test_remove7(self):
+        """Test removing an item of value list, tuple, dict."""
+        bag = ListBag([[True, False], (1, 2), {33: "Name"}])
+        self.assertEqual(bag.remove([True, False]), [True, False])
+        self.assertEqual(bag.remove((1, 2)), (1, 2))
+        self.assertEqual(bag.remove({33: "Name"}), {33: "Name"})
+
+
+class GrabTestCase(unittest.TestCase):
+
+    def test_grab1(self):
+        """Test granning an item from an empty bag."""
+        bag = ListBag()
+        with self.assertRaises(ValueError):
+            bag.grab()
+
+    def test_grab2(self):
+        """Test granning an item with int values."""
+        bag = ListBag([3, 3, 3])
+        init_len = len(bag)
+        grabbed_item = bag.grab()
+        self.assertEqual(grabbed_item, 3)
+        self.assertEqual(len(bag), init_len - 1)
+
+    def test_grab3(self):
+        """Test granning an item with float, 'double' values."""
+        bag = ListBag([3.3, 3.3, 3.3])
+        init_len = len(bag)
+        grabbed_item = bag.grab()
+        self.assertEqual(grabbed_item, 3.3)
+        self.assertEqual(len(bag), init_len - 1)
+
+    def test_grab4(self):
+        """Test granning an item with char, string values."""
+        bag1 = ListBag(['a', 'a', 'a'])
+        init_len = len(bag1)
+        grabbed_item = bag1.grab()
+        self.assertEqual(grabbed_item, 'a')
+        self.assertEqual(len(bag1), init_len - 1)
+
+        bag2 = ListBag(["Hi", "Hi", "Hi"])
+        init_len = len(bag2)
+        grabbed_item = bag2.grab()
+        self.assertEqual(grabbed_item, "Hi")
+        self.assertEqual(len(bag2), init_len - 1)
+
+    def test_grab5(self):
+        """Test granning an item with lists, tuples, dict."""
+        bag1 = ListBag([[1, 2], [1, 2], [1, 2]])
+        init_len = len(bag1)
+        grabbed_item = bag1.grab()
+        self.assertEqual(grabbed_item, [1, 2])
+        self.assertEqual(len(bag1), init_len - 1)
+
+        bag2 = ListBag([(1, 2), (1, 2), (1, 2)])
+        init_len = len(bag2)
+        grabbed_item = bag2.grab()
+        self.assertEqual(grabbed_item, (1, 2))
+        self.assertEqual(len(bag2), init_len - 1)
+
+        bag3 = ListBag([{1: 2}, {1: 2}, {1: 2}])
+        init_len = len(bag3)
+        grabbed_item = bag3.grab()
+        self.assertEqual(grabbed_item, {1: 2})
+        self.assertEqual(len(bag3), init_len - 1)
+
+
+class DunderAddTestCase(unittest.TestCase):
+
+    def test_dunder_add1(self):
+        """Test dunder add with elems that are not Listbags."""
+        bag = ListBag([1, 5, 6, 8])
+        not_bag = "Hi"
+        with self.assertRaises(TypeError):
+            bag3 = bag + not_bag
+
+    def test_dunder_add2(self):
+        """Test dunder add with empty ListBags."""
+        bag1 = ListBag([1, 5, 6, 8])
+        bag2 = ListBag()
+        bag3 = bag1 + bag2
+        bag1 = ListBag([1, 5, 6, 8])
+        bag2 = ListBag()
+        bag3 = bag1 + bag2
+        self.assertEqual(bag3, ListBag([1, 5, 6, 8]))
+
+        bag4 = ListBag()
+        bag5 = ListBag()
+        bag6 = bag4 + bag5
+        self.assertEqual(bag6, ListBag([]))
+
+    def test_dunder_add3(self):
+        """Test dunder add with ListBags (int values)."""
+        bag1 = ListBag([1, 5, 6, 8])
+        bag2 = ListBag([40, 80, 90])
+        bag3 = bag1 + bag2
+        self.assertEqual(bag3, ListBag([1, 5, 6, 8, 40, 80, 90]))
+
+    def test_dunder_add4(self):
+        """Test dunder add with ListBags (float, 'double' values)."""
+        bag1 = ListBag([1.0, 5.0, 6.0, 8.0])
+        bag2 = ListBag([40.0])
+        bag3 = bag1 + bag2
+        self.assertEqual(bag3, ListBag([1.0, 5.0, 6.0, 8.0, 40.0]))
+
+    def test_dunder_add5(self):
+        """Test dunder add with ListBags (char, string values)."""
+        bag1 = ListBag(['a', "Hi"])
+        bag2 = ListBag(["Bee"])
+        bag3 = bag1 + bag2
+        self.assertEqual(bag3, ListBag(['a', "Hi", "Bee"]))
+
+    def test_dunder_add6(self):
+        """Test dunder add with ListBags (lists, tuples, dict values)."""
+        bag1 = ListBag([[1, 2], (1, 2)])
+        bag2 = ListBag([{1: 2}])
+        bag3 = bag1 + bag2
+        self.assertEqual(bag3, ListBag([[1, 2], (1, 2), {1: 2}]))
+
+
+class EqTestCase(unittest.TestCase):
+
+    def test_eq1(self):
+        """Test __eq__ with objects that are not Listbags."""
+        bag1 = ListBag([1, 5, 6, 8])
+        not_bag1 = "Hi"
+        self.assertFalse(bag1 == not_bag1)
+
+        bag2 = ListBag([1, 5, 6, 8])
+        not_bag2 = [1, 5, 6, 8]
+        self.assertFalse(bag2 == not_bag2)
+
+    def test_eq2(self):
+        """Test __eq__ with objects that are Listbags."""
+        bag1 = ListBag([1, 5, 6, 8])
+        bag2 = ListBag([1, 5, 6, 8])
+        bag3 = bag2
+        bag4 = ListBag([40, 80, 90])
+        bag5 = ListBag(['a', "Hello"])
+        bag6 = bag5
+        self.assertTrue(bag1 == bag1)
+        self.assertTrue(bag1 == bag2)
+        self.assertTrue(bag1 == bag3)
+        self.assertTrue(bag2 == bag3)
+        self.assertFalse(bag1 == bag4)
+        self.assertFalse(bag4 == bag5)
+        self.assertTrue(bag5 == bag6)
+
+    def test_eq3(self):
+        """Test __eq__ with Listbags and order of elements."""
+        bag1 = ListBag([1, 5, 6, 8])
+        bag2 = ListBag([8, 5, 6, 1])
+        bag3 = ListBag([1.1, 2.2, 3.3])
+        bag4 = ListBag([1.1, 3.3, 2.2])
+        bag5 = ListBag(['a', 'b'])
+        bag6 = ListBag(['b', 'a'])
+        bag7 = ListBag([[1, 2], (1, 2), {1: 2}])
+        bag8 = ListBag([{1: 2}, [1, 2], (1, 2)])
+        self.assertTrue(bag1 == bag2)
+        self.assertTrue(bag3 == bag4)
+        self.assertTrue(bag5 == bag6)
+        self.assertTrue(bag7 == bag8)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
